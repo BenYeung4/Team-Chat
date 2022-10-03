38e46aa496fe76b668fc46d166db8982848a48bd
Ben <byeungis@gmail.com>
2022-10-03 19:47:44 -0400

    setting up the navbar and sidebar with other users

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar">
+      <span className="logo"> Team-Chat</span>
+      <div className="user">
+        <img
+          src="https://img.icons8.com/stickers/100/000000/pokemon.png"
+          alt=""
+        ></img>
+        <span className="username"> User</span>
+        <button> Logout</button>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,7 +1,22 @@
 import React from "react";
 
 function Search() {
-  return <div>Search</div>;
+  return (
+    <div className="search">
+      <div className="searchForm">
+        <input type="text" placeholder="Search for a user" />
+      </div>
+      <div className="userChat">
+        <img
+          src="https://img.icons8.com/stickers/100/000000/pokemon.png"
+          alt=""
+        />
+        <div className="userChatInfo">
+          <span>Pikachu</span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Search;
