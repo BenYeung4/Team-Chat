894c8c2c3b0c7fd235a0b2c5c148fdcef816d915
Ben <byeungis@gmail.com>
2022-10-03 21:30:16 -0400

    adjusted navbar

--- client/src/components/Chats.js
+++ client/src/components/Chats.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 function Chats() {
-  return <div>Chats</div>;
+  return (
+    <div className="userChat">
+      <img
+        src="https://img.icons8.com/stickers/100/000000/pokemon.png"
+        alt=""
+      />
+      <div className="userChatInfo">
+        <span>Pikachu</span>
+        <p>hi</p>
+      </div>
+    </div>
+  );
 }
 
 export default Chats;
