9ba3e49bf18672f977634543cc37c11e12643685
Ben <byeungis@gmail.com>
2022-10-07 23:45:51 -0400

    rough draft of front end design

--- client/src/components/Message.js
+++ client/src/components/Message.js
@@ -1,7 +1,26 @@
 import React from "react";
 
 function Message() {
-  return <div>Message</div>;
+  return (
+    <div className="message owner">
+      {/* displaying the time when messages have been sent/recieved */}
+      <div className="messageInfo">
+        <img
+          src="https://img.icons8.com/stickers/100/000000/pokemon.png"
+          alt=""
+        />
+        <span> just now</span>
+      </div>
+      {/* user's message */}
+      <div className="messageContent">
+        <p>First message</p>
+        <img
+          src="https://img.icons8.com/stickers/100/000000/pokemon.png"
+          alt=""
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Message;
