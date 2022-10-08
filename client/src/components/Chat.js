b841b7c303fd393df9f78dda225d8acf7a76f77a
Ben <byeungis@gmail.com>
2022-10-07 21:26:09 -0400

    update on messages and readme

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -1,7 +1,25 @@
 import React from "react";
+import Cam from "../img/cam.png";
+import Join from "../img/join.png";
+import More from "../img/more.png";
+import Messages from "./Messages";
+import Input from "./Input";
 
 function Chat() {
-  return <div className="chat">Chat</div>;
+  return (
+    <div className="chat">
+      <div className="chatInfo">
+        <span>Pikachu username</span>
+        <div className="chatIcons">
+          <img src={Cam} alt="" />
+          <img src={Join} alt="" />
+          <img src={More} alt="" />
+        </div>
+      </div>
+      <Messages />
+      <Input />
+    </div>
+  );
 }
 
 export default Chat;
--- client/src/components/Chats.js
+++ client/src/components/Chats.js
@@ -2,14 +2,26 @@ import React from "react";
 
 function Chats() {
   return (
-    <div className="userChat">
-      <img
-        src="https://img.icons8.com/stickers/100/000000/pokemon.png"
-        alt=""
-      />
-      <div className="userChatInfo">
-        <span>Pikachu</span>
-        <p>hi</p>
+    <div className="chats">
+      <div className="userChat">
+        <img
+          src="https://img.icons8.com/stickers/100/000000/pokemon.png"
+          alt=""
+        />
+        <div className="userChatInfo">
+          <span>Pikachu</span>
+          <p>displaying mesage on tab</p>
+        </div>
+      </div>
+      <div className="userChat">
+        <img
+          src="https://img.icons8.com/stickers/100/000000/pokemon.png"
+          alt=""
+        />
+        <div className="userChatInfo">
+          <span>Pikachu</span>
+          <p>displaying second message</p>
+        </div>
       </div>
     </div>
   );
